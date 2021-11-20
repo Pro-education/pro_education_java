6aa5df39eca1bb7eb9e2712898aacc36b7907468
Vadim Ershov <vadim.ershov.01@mail.ru>
2021-11-20 22:57:38 +0300

    try some tests

---
+++ src/test/java/ru/ershov/pro_education/service/clazz/TestAbstractDto.java
@@ -0,0 +1,14 @@
+package ru.ershov.pro_education.service.clazz;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.ershov.pro_education.dto.AbstractDto;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public
+class TestAbstractDto extends AbstractDto {
+    private String name;
+}
