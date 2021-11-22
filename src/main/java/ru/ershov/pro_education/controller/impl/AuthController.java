b51eed37d9357ef2c2f46540d4e4f865b2f79402
Vadim Ershov <vadim.ershov.01@mail.ru>
2021-11-22 21:47:48 +0300

    realise security with role

--- src/main/java/ru/ershov/pro_education/controller/impl/AuthController.java
+++ src/main/java/ru/ershov/pro_education/controller/impl/AuthController.java
@@ -2,38 +2,59 @@ package ru.ershov.pro_education.controller.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.ershov.pro_education.config.jwt.JwtProvider;
+import ru.ershov.pro_education.config.jwt.TokenProvider;
 import ru.ershov.pro_education.entity.User;
 import ru.ershov.pro_education.service.impl.UserServiceImpl;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthenticationManager authenticationManager;
+    private final TokenProvider jwtTokenUtil;
     private final UserServiceImpl userService;
-    private final JwtProvider jwtProvider;
 
     @PostMapping("/register")
     public String registerUser(@RequestBody @Valid RegistrationRequest registrationRequest) {
         User u = new User();
         u.setPassword(registrationRequest.getPassword());
         u.setEmail(registrationRequest.getLogin());
-        u.setRoleId(1L);
         u.setEnabled(true);
         userService.insert(u);
         return "OK";
     }
 
-    @PostMapping("/auth")
-    public ResponseEntity<String> auth(@RequestBody AuthRequest request) {
-        User user = userService.getUser(request.getLogin());
-        String body = jwtProvider.generateToken(user.getEmail());
-        return ResponseEntity.ok(body);
+    @PostMapping(value = "/authenticate")
+    public String generateToken(@RequestBody User loginUser) throws AuthenticationException {
+
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginUser.getEmail(),
+                        loginUser.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return jwtTokenUtil.generateToken(authentication);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping(value="/userping") // test
+    public String userPing(){
+        return "Any User Can Read This";
     }
 
 }
